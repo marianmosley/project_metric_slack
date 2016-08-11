39b58dd0cdc6e85207c1c0956dce653e15f2beb9
mtc2013 <mtc2013opensource@gmail.com>
2016-08-11 17:24:31 -0400

    Make Spec pass

--- spec/project_metric_slack_spec.rb
+++ spec/project_metric_slack_spec.rb
@@ -4,6 +4,6 @@ describe ProjectMetricSlack, :vcr do
   it 'it gets raw data' do
     metric = ProjectMetricSlack.new(channel: 'projectscope')
     metric.refresh
-    expect(metric.raw_data).to eq "stuff"
+    expect(metric.raw_data).to eq({"armandofox"=>5, "francis"=>0, "mtc2013"=>2, "tansaku"=>10})
   end
 end
